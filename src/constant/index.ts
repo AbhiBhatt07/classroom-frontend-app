962f4718ba8a86b41c4b117ec008c29892b56581
Abhishek Bhatt <abhibhatt587@gmail.com>
2026-01-22 11:50:14 +0530

    Implement data provider and displaying data on the screen

---
+++ src/constant/index.ts
@@ -0,0 +1,28 @@
+export const DEPARTMENTS = [
+  'all',
+  'Computer Science',
+  'Mathematics',
+  'Physics',
+  'Chemistry',
+  'Biology',
+  'Geography',
+  'History',
+  'English',
+  'French',
+  'German',
+  'Italian',
+  'Spanish',
+  'Chinese',
+  'Japanese',
+  'Korean',
+  'Arabic',
+  'Hindi',
+  'Urdu',
+  'Portuguese',
+  'Russian',
+]
+
+export const DEPEARTMENT_OPTION = DEPARTMENTS.map((department) => ({
+  value: department,
+  label: department
+}))
